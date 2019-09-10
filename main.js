4cf203a20f1e82af5ba40358bd05016fd577cf4f
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-10 12:02:41 -0400

    trying to use javascript

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 
-function time() {
-  var date=new Date();
-  var hours=date.getHours().toString(16);
-  var minutes=date.getMinutes().toString(16);
-  var seconds=date.getSeconds().toString(16);
+    (function() {
+        "use strict";
 
- var arr=[hours,minutes,seconds].map(function(num){
-      return (num<10) ? '0'+num : String(num); });
+        var currentTime, hours, minutes, seconds, hexHours, hexMinutes, hexSeconds, progress;
 
-      hours=arr[0];
-      minutes=arr[1];
-      seconds=arr[2];
+        var $colorClock = document.querySelector(".color-clock");
+        var $colorClockText = document.querySelector(".color-clock-text");
+        var $colorClockProgressBar = document.querySelector(".color-clock_progress-bar");
 
-      return hours + ' : ' +  minutes + ' : ' + seconds;
-}
+        var isHovering = false;
 
-function output(time)
-{
-    document.querySelector('.time').textContent=time;
-    let color= ' # ' + time;
-    document.body.bgColor=color;
+        $colorClock.addEventListener("mouseover", function() {
+            isHovering = true;
 
-}
+            isHovering = !isHovering;
+        });
+
+        $colorClock.addEventListener("mouseout" , function() {
+            isHovering = false;
+        });
 
 
+        setInterval(function() {
+            if(isHovering= true){
 
-setInterval( function()
-{
-	output( time() )
-} , 1000 );
+                currentTime = new Date();
+
+                hexHours = ("0" + currentTime.getHours().toString(16)).slice(-2);
+                hexMinutes = ("0" + currentTime.getMinutes().toString(16)).slice(-2);
+                hexSeconds = ("0" + currentTime.getSeconds().toString(16)).slice(-2);
+
+                $colorClockText.textContent = hours + ":" + minutes + ":" + seconds;
+            }
+            else{
+                currentTime = new Date();
+                hours = ("0" + currentTime.getHours()).slice(-2);
+                minutes = ("0" + currentTime.getMinutes()).slice(-2);
+                seconds = ("0" + currentTime.getSeconds()).slice(-2);
+
+                $colorClockText.textContent = hours + ":" + minutes + ":" + seconds;
+        }
+
+        },1000);
+    })();
