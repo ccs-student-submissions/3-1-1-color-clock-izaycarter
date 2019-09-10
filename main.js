295e808034e8e6feb21caaaea82bf8141d94b27d
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-10 12:48:53 -0400

    next is seconds line moving

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-    (function() {
+(function() {
         "use strict";
 
         var currentTime, hours, minutes, seconds, hexHours, hexMinutes, hexSeconds, progress;
@@ -22,18 +21,16 @@
 
 
         setInterval(function() {
-            if(isHovering= true){
+            currentTime = new Date();
 
-                currentTime = new Date();
-
+            if(isHovering === true){
                 hexHours = ("0" + currentTime.getHours().toString(16)).slice(-2);
                 hexMinutes = ("0" + currentTime.getMinutes().toString(16)).slice(-2);
                 hexSeconds = ("0" + currentTime.getSeconds().toString(16)).slice(-2);
 
                 $colorClockText.textContent = hours + ":" + minutes + ":" + seconds;
-            }
-            else{
-                currentTime = new Date();
+            } else {
+
                 hours = ("0" + currentTime.getHours()).slice(-2);
                 minutes = ("0" + currentTime.getMinutes()).slice(-2);
                 seconds = ("0" + currentTime.getSeconds()).slice(-2);
